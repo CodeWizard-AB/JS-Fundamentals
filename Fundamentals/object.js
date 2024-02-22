ede3f258b5ff940737db83cf9bb8e33596517d76
Anunay Argha <anunayargha@gmail.com>
2024-02-22 22:50:05 +0600

    object methods

--- Fundamentals/object.js
+++ Fundamentals/object.js
@@ -63,3 +63,10 @@ Object.values(person).forEach((value) =>
 console.log(Object.keys(person));
 console.log(Object.values(person));
 console.log(Object.entries(person));
+
+person.isRich = true;
+
+console.log(person);
+
+Object.freeze(person);
+
