52938da2294013243bbe47845d980b4794b0ed5e
Anunay Argha <anunayargha@gmail.com>
2024-02-23 10:57:11 +0600

    class object

--- Fundamentals/class.js
+++ Fundamentals/class.js
@@ -7,12 +7,6 @@ class Product {
 		this.buyingStore = store;
 		this.productPrice = price;
 	}
-
-	productDetail() {
-		{
-			this.processor, this.productName, this.productPrice, this.buyingStore;
-		}
-	}
 }
 
 const product1 = new Product("m3", "macbook", 2500, "apple");
@@ -20,3 +14,39 @@ const product1 = new Product("m3", "macbook", 2500, "apple");
 console.log(product1);
 
 for (const key in product1) console.log(key, product1[key]);
+
+class Person {
+	constructor(name, age, position) {
+		this.name = name;
+		this.age = age;
+		this.position = position;
+	}
+}
+
+const teachers = [
+	new Person("Nodi", 28, "Senior"),
+	new Person("Akil", 26, "Junior"),
+	new Person("Shobuj", 30, "Senior"),
+];
+
+const teacherNames = teachers
+	.filter(({ position }) => position === "Senior")
+	.map(({ name }) => name);
+
+const totalAge = teachers.reduce((total, { age }) => total + age, 0);
+
+console.log(totalAge);
+console.log(teacherNames);
+
+// * THIS KEYWORD -
+
+const teacher = {
+	firstName: "John",
+	lastName: "Doe",
+	age: 34,
+	fullName() {
+		return `${this.firstName} ${this.lastName}`;
+	},
+};
+
+console.log(teacher.fullName());
