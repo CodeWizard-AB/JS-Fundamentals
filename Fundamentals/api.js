0f3ccbf25ccb126dd5954a63cdf433a046ef1102
Anunay Argha <anunayargha@gmail.com>
2024-02-26 21:20:46 +0600

    api call

--- Fundamentals/api.js
+++ Fundamentals/api.js
@@ -1,1 +1,9 @@
 "use strict";
+
+const url = "https://jsonplaceholder.typicode.com/todos/1";
+
+const loadFunction = () => {
+	const data = fetch(url)
+		.then((response) => response.json())
+		.then((json) => console.log(json));
+};
