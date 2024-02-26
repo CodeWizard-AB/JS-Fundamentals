fa903063f6edd50565b1d1cc2c6fd328b131773a
Anunay Argha <anunayargha@gmail.com>
2024-02-26 22:36:46 +0600

    js fetch

--- Fundamentals/api.js
+++ Fundamentals/api.js
@@ -1,9 +1,8 @@
 "use strict";
 
-const url = "https://jsonplaceholder.typicode.com/todos/1";
-
 const loadFunction = () => {
-	const data = fetch(url)
+	const url = "https://jsonplaceholder.typicode.com/todos/1";
+	fetch(url)
 		.then((response) => response.json())
 		.then((json) => console.log(json));
 };
