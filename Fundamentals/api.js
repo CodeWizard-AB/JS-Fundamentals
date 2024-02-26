feea506bf5e44817b80a17debf87f8d2b1380a8f
Anunay Argha <anunayargha@gmail.com>
2024-02-26 23:27:20 +0600

    api refresh

--- Fundamentals/api.js
+++ Fundamentals/api.js
@@ -1,8 +1,31 @@
 "use strict";
 
 const loadFunction = () => {
-	const url = "https://jsonplaceholder.typicode.com/todos/1";
+	const url = "https://jsonplaceholder.typicode.com/users";
 	fetch(url)
 		.then((response) => response.json())
-		.then((json) => console.log(json));
+		.then((json) => displayData(json));
+};
+
+const displayData = function (data) {
+	const userContainer = document.getElementById("user_container");
+	data.forEach((user) => {
+		const li = `<li>${user.username}</li>`;
+		userContainer.insertAdjacentHTML("beforeend", li);
+	});
+};
+
+const loadPhotos = () => {
+	const url = "https://jsonplaceholder.typicode.com/photos";
+	fetch(url)
+		.then((res) => res.json())
+		.then((json) => displayPhotos(json));
+};
+
+const displayPhotos = (data) => {
+	const photoContainer = document.getElementById("photos_container");
+	data.slice(0, 3).forEach((photo) => {
+		const image = `	<img src=${photo.url}>`;
+		photoContainer.insertAdjacentHTML("beforeend", image);
+	});
 };
